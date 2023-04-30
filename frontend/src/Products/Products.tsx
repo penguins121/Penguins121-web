f5076987f823d466b017c7114fe4c08ee682d0ae
Tom Kennedy <holtsrule1@gmail.com>
2023-04-30 02:24:50 -0400

    Catalog feature

--- frontend/src/Products/Products.tsx
+++ frontend/src/Products/Products.tsx
@@ -1,7 +1,23 @@
+import data from "./data"
+
 function Products() {
     return (
-        <div>
-            <h2>Products</h2>
+        <div className="content">
+            <ul className="products">
+                {data.products.map((Product) => (
+                    <li>
+                        <div className="product">
+                            <img className="product-image" src={Product.imageUrl} alt="product" />
+                            <div className="product-name">
+                                <a href="product.html">{Product.name}</a>
+                            </div>
+                            <div className="product-brand">{Product.brand}</div>
+                            <div className="product-price">{Product.price}</div>
+                            <div className="product-rating">{Product.rating} Stars ({Product.numOfReviews} Reviews)</div>
+                        </div>
+                    </li>
+                ))};
+            </ul>
         </div>
     );
 }
