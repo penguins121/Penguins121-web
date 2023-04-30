51bee14e029763d573fc2f48cef47122e7380cc6
Collin McGough <123339962+CollinMcGough@users.noreply.github.com>
2023-04-30 19:12:16 +0000

    modified:   frontend/src/App.test.tsx

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -2,8 +2,22 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('The main app',()=> {
+  
+  it('should render the Orders link', () => {
+    render(<App />);
+    expect(screen.getByText('Orders')).toBeInTheDocument ();
+  });
+
+  it('should render the Home link', () => {
+    render(<App />);
+    // expect(screen.getAllByText('Home')).toBeInTheDocument();
+    expect(screen.getByRole('link', {name: "Home"})).toBeInTheDocument ();
+  });
+
+  it('should render the Home header', () => {
+    render(<App />);
+    expect(screen.getByRole('heading', {level: 2})).toBeInTheDocument ();
+  });
+
 });
